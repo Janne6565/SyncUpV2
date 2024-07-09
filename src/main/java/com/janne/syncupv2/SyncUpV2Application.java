0d1d46d0d79a5abb1bf5912d9f2552363d6596d4
Janne Keipert <jannekeipert@gmx.de>
2024-07-09 18:38:22 +0200

    add base structure

---
+++ src/main/java/com/janne/syncupv2/SyncUpV2Application.java
@@ -0,0 +1,13 @@
+package com.janne.syncupv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncUpV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncUpV2Application.class, args);
+    }
+
+}
