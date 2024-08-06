e57922907f1b8bdfedc87dd487e85609fc7bc850
janne6565 <jabbekeipert@gmail.com>
2024-08-06 22:07:02 +0200

    change imageCollection id type to string (uuid generation)

--- src/main/java/com/janne/syncupv2/model/dto/outgoing/post/ImageCollectionDto.java
+++ src/main/java/com/janne/syncupv2/model/dto/outgoing/post/ImageCollectionDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 public class ImageCollectionDto {
-    private Long id;
+    private String id;
     private ScaledImageDto imageLooking;
     private ScaledImageDto imageStanding;
     private ScaledImageDto imageLanding;
